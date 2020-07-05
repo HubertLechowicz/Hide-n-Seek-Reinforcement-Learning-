9226e4287ef16c3cc0d3f6f4e1462dc1c61924e5
QuatZo <48104274+QuatZo@users.noreply.github.com>
2020-07-05 15:10:19 +0200

    Animations

--- hidenseek/objects/controllable.py
+++ hidenseek/objects/controllable.py
@@ -1,5 +1,6 @@
 import pygame
 from ext.supportive import Point
+import copy
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, width, height, speed, pos_ratio, color, SCREEN_WIDTH, SCREEN_HEIGHT):
@@ -15,22 +16,36 @@ class Player(pygame.sprite.Sprite):
 
         self.velocity = speed # how fast it should move
 
-        image = pygame.Surface((width, height))
-        image.fill((0, 0, 0, 0))
-        image.set_colorkey((0, 0, 0))
+        image_inplace = pygame.Surface((width, height))
+        image_inplace.fill((0, 0, 0, 0))
+        image_inplace.set_colorkey((0, 0, 0))
 
         pygame.draw.rect(
-            image,
+            image_inplace,
             color,
             (0, 0, width, height),
             0
         )
 
-        self.image = image
+        self.sprite_index = 0
+        image_movement = pygame.Surface((width, height))
+        image_movement.fill((0, 0, 0, 0))
+        image_movement.set_colorkey((0, 0, 0))
+
+        pygame.draw.rect(
+            image_movement,
+            (64, 128, 240),
+            (0, 0, width, height),
+            0
+        )
+
+        self.images = [image_inplace] + [image_movement for _ in range(10)]
+        self.image = image_inplace
         self.rect = self.image.get_rect()
         self.rect.center = (self.pos_init.x, self.pos_init.y)
 
     def update(self, movement, dt):
+        old_pos = copy.deepcopy(self.pos)
         self.pos += self.velocity * Point((movement.x * dt, movement.y * dt))
 
         if self.pos.y - self.height / 2 <= 0:
@@ -46,3 +61,7 @@ class Player(pygame.sprite.Sprite):
             self.pos.x = self.SCREEN_WIDTH - self.width / 2
 
         self.rect.center = (self.pos.x, self.pos.y)
+
+        if old_pos != self.pos:
+            self.sprite_index = (self.sprite_index + 1) % len(self.images)
+            self.image = self.images[self.sprite_index]
